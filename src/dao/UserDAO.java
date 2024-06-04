916eb151d58e7cc9357973a1e550b1141eee45ee
stanleyshi42 <stanleyshi42@gmail.com>
2024-06-04 12:24:57 -0700

    added getUser query

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -2,11 +2,36 @@ package dao;
 
 import java.sql.*;
 
+import model.User;
+
 public class UserDAO {
 
 	final static String DB_URL = "jdbc:mysql://localhost:3306/inventory_management";
 	final static String DB_USER = "root";
 	final static String DB_PASSWORD = "root";
+	
+	public static User getUser(String username) {
+		
+		try {
+			Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+			PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM user WHERE username = ?");
+			preparedStatement.setString(1, username);
+
+			ResultSet rs = preparedStatement.executeQuery();
+			if (rs.next()) {
+				int id = rs.getInt("id");
+				String password = rs.getString("password");
+				String role = rs.getString("role");
+				return new User(id, username, password, role);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+
+		return null;
+	}
 
 	// Checks login credentials
 	public static boolean authenticateUser(String username, String password) {
