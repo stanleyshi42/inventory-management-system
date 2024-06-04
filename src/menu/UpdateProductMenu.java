e6ca17f67240e6e789be28bb33a7a527c652b874
stanleyshi42 <stanleyshi42@gmail.com>
2024-06-04 16:27:34 -0700

    added a check for valid product ID

--- src/menu/UpdateProductMenu.java
+++ src/menu/UpdateProductMenu.java
@@ -4,33 +4,39 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import dao.ProductDAO;
+import model.Product;
 
 public class UpdateProductMenu {
+
 	static void run(Scanner sc) {
 		try {
 			System.out.println("Enter product ID:");
 			int idInput = sc.nextInt();
 			sc.nextLine();
 
-			System.out.println("Enter new price:");
-			float priceInput = sc.nextFloat();
-			sc.nextLine();
+			Product product = ProductDAO.getProduct(idInput);
 
-			System.out.println("Enter new quantity:");
-			int quantityInput = sc.nextInt();
-			sc.nextLine();
+			if (product == null) {
+				System.out.println("Operation failed: invalid ID");
 
-			System.out.println("Enter new name:");
-			String nameInput = sc.nextLine();
+			} else {
+				System.out.println("Enter new price:");
+				float priceInput = sc.nextFloat();
+				sc.nextLine();
 
-			if (ProductDAO.updateProduct(idInput, priceInput, quantityInput, nameInput))
-				System.out.println("Product successfully updated");
-			else
-				System.out.println("Operation failed");
+				System.out.println("Enter new quantity:");
+				int quantityInput = sc.nextInt();
+				sc.nextLine();
 
-			System.out.println("Hit enter to return");
-			sc.nextLine();
-			return;
+				System.out.println("Enter new name:");
+				String nameInput = sc.nextLine();
+
+				if (ProductDAO.updateProduct(idInput, priceInput, quantityInput, nameInput))
+					System.out.println("Product successfully updated");
+				else
+					System.out.println("Operation failed");
+
+			}
 
 		} catch (InputMismatchException e) {
 			System.out.println("Invalid input");
@@ -38,5 +44,9 @@ public class UpdateProductMenu {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+		System.out.println("Hit enter to return");
+		sc.nextLine();
+
 	}
 }
