88eaeb7676467919db62030c3b701d8abd9cc7a2
stanleyshi42 <stanleyshi42@gmail.com>
2024-06-04 14:16:15 -0700

    implemented adding sales

---
+++ src/menu/AddSaleMenu.java
@@ -0,0 +1,33 @@
+package menu;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import dao.SaleDAO;
+
+public class AddSaleMenu {
+
+	public static void run(Scanner sc) {
+		try {
+			System.out.println("Enter product ID:");
+			int idInput = sc.nextInt();
+			sc.nextLine();
+
+			System.out.println("Enter quantity:");
+			int quantityInput = sc.nextInt();
+			sc.nextLine();
+			
+			SaleDAO.addSale(idInput, quantityInput);
+
+			System.out.println("Hit enter to return");
+			sc.nextLine();
+			return;
+
+		} catch (InputMismatchException e) {
+			System.out.println("Invalid input");
+			sc.nextLine();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
