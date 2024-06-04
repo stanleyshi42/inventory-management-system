eb453be38de0a8b4f42bf72e56b7650822b03ea4
stanleyshi42 <stanleyshi42@gmail.com>
2024-06-04 14:16:44 -0700

    implemented adding sales

--- src/dao/SaleDAO.java
+++ src/dao/SaleDAO.java
@@ -1,5 +1,28 @@
 package dao;
 
+import java.sql.*;
+
+import menu.Helper;
+import model.Sale;
+
 public class SaleDAO {
 
+	// TODO check if product ID is valid
+	public static boolean addSale(int product_id, int quantity) {
+		try {
+			Connection connection = DriverManager.getConnection(Helper.DB_URL, Helper.DB_USER, Helper.DB_PASSWORD);
+			PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO sale (product_id, quantity) VALUES (?, ?)");
+			preparedStatement.setInt(1, product_id);
+			preparedStatement.setInt(2, quantity);
+			preparedStatement.executeUpdate();
+
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+
+		return false;
+
+	}
 }
