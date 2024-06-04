8751de5ca3ee245d898d404b31b3ce685fd5da9e
stanleyshi42 <stanleyshi42@gmail.com>
2024-06-04 13:27:54 -0700

    added files

--- src/menu/AddProductMenu.java
+++ src/menu/AddProductMenu.java
@@ -1,5 +1,22 @@
 package menu;
 
-public class AddProductMenu {
+import java.util.Scanner;
 
+import model.User;
+
+public class AddProductMenu {
+	static void run(Scanner sc, User user) {
+
+		while (true) {
+			try {
+				int input = sc.nextInt();
+				sc.nextLine();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+
+			}
+
+		}
+	}
 }
